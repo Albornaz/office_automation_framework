87d70cd5e9730fafcc11e10c8b3a8de07941a25d
JR.dev <180821520+mscbuild@users.noreply.github.com>
2025-12-30 16:19:35 +0200

    Delete security directory

--- security/access.py
+++
@@ -1,7 +0,0 @@
-class AccessControl:
-    LEVELS = {"public", "internal", "confidential"}
-
-    @staticmethod
-    def validate(level):
-        if level not in AccessControl.LEVELS:
-            raise ValueError("Invalid access level")
